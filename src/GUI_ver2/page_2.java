098fe1a3fae4f5b60e60b512e270c049c27b5c27
Sarah <2234sarah@gmail.com>
2021-12-03 02:11:19 +0900

    배경 및 버튼 추가

--- src/GUI_ver2/page_2.java
+++ src/GUI_ver2/page_2.java
@@ -90,33 +90,96 @@ class explain extends JPanel{
 }
 
 public class page_2 extends JPanel{
-    public page_2(){
-        JPanel panel3 = new JPanel();
+    private Image image; // 배경화면
+
+    public page_2() {
         JPanel panel4 = new JPanel(new GridLayout());
         JPanel main = new JPanel();
-        JButton img;
-        JButton img_2;
+
+        JPanel star_list = new JPanel();
+        star_list.setBounds(0, 0, 450, 600);
+        this.add(star_list);
+
+        star_list.setLayout(null);
+
         //사진크기를 조정 -> 함수로 생성해야할 필요 있음(사진을 버튼 크기에 맞추려면)
         ImageIcon Cancer = new ImageIcon("src\\GUI_ver2\\image\\게자리.jpg");
         Image Cancer_img = Cancer.getImage();
-        Image Change = Cancer_img.getScaledInstance(100,105,Image.SCALE_SMOOTH);
+        Image Change = Cancer_img.getScaledInstance(100, 105, Image.SCALE_SMOOTH);
         ImageIcon Change_Cancer = new ImageIcon(Change);
 
         ImageIcon Leo = new ImageIcon("src\\GUI_ver2\\image\\사자자리.png");
         Image Leo_img = Leo.getImage();
-        Image Change_2 = Leo_img.getScaledInstance(100,105,Image.SCALE_SMOOTH);
+        Image Change_2 = Leo_img.getScaledInstance(100, 105, Image.SCALE_SMOOTH);
         ImageIcon Change_Leo = new ImageIcon(Change_2);
 
-        //버튼에 사진 넣고 버튼 크기 조정
-        img = new JButton(Change_Cancer);
-        img_2 = new JButton(Change_Leo);
-        img.setPreferredSize(new Dimension(100,105));
-        img_2.setPreferredSize(new Dimension(100,105));
+        JButton btnNewButton = new JButton(Change_Cancer);
+        btnNewButton.setBounds(36, 10, 100, 105);
+        star_list.add(btnNewButton);
 
-        panel3.add(img);
-        panel3.add(img_2);
+        JButton btnNewButton_1 = new JButton(Change_Leo);
+        btnNewButton_1.setBounds(159, 10, 100, 105);
+        star_list.add(btnNewButton_1);
 
-        img.addActionListener(new ActionListener() {
+        JButton btnNewButton_2 = new JButton("New button");
+        btnNewButton_2.setBounds(283, 10, 100, 105);
+        star_list.add(btnNewButton_2);
+
+        JButton btnNewButton_3 = new JButton("New button");
+        btnNewButton_3.setBounds(36, 125, 100, 105);
+        star_list.add(btnNewButton_3);
+
+        JButton btnNewButton_4 = new JButton("New button");
+        btnNewButton_4.setBounds(159, 125, 100, 105);
+        star_list.add(btnNewButton_4);
+
+        JButton btnNewButton_5 = new JButton("New button");
+        btnNewButton_5.setBounds(283, 125, 100, 105);
+        star_list.add(btnNewButton_5);
+
+        JButton btnNewButton_6 = new JButton("New button");
+        btnNewButton_6.setBounds(36, 240, 100, 105);
+        star_list.add(btnNewButton_6);
+
+        JButton btnNewButton_7 = new JButton("New button");
+        btnNewButton_7.setBounds(159, 240, 100, 105);
+        star_list.add(btnNewButton_7);
+
+        JButton btnNewButton_8 = new JButton("New button");
+        btnNewButton_8.setBounds(283, 240, 100, 105);
+        star_list.add(btnNewButton_8);
+
+        JButton btnNewButton_6_1 = new JButton("New button");
+        btnNewButton_6_1.setBounds(36, 355, 100, 105);
+        star_list.add(btnNewButton_6_1);
+
+        JButton btnNewButton_6_2 = new JButton("New button");
+        btnNewButton_6_2.setBounds(159, 355, 100, 105);
+        star_list.add(btnNewButton_6_2);
+
+        JButton btnNewButton_6_3 = new JButton("New button");
+        btnNewButton_6_3.setBounds(283, 355, 100, 105);
+        star_list.add(btnNewButton_6_3);
+
+        // 버튼 액션 달기
+
+        // 배경 넣기
+        image = new ImageIcon("src\\GUI_ver2\\image\\login_background_1.jpg").getImage();
+        JPanel background = new JPanel() {
+            public void paintComponent(Graphics g) {
+                Dimension d = getSize();
+                g.drawImage(image, 0, 0, d.width, d.height, null);
+
+                setOpaque(false); // 그림 투명도
+                super.paintComponent(g);
+            }
+        };
+        background.setBounds(0, 0, 450, 600);
+        star_list.add(background);
+
+    ////////////////
+
+        btnNewButton_1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 panel4.removeAll();
@@ -127,7 +190,7 @@ public class page_2 extends JPanel{
             }
         });
 
-        img_2.addActionListener(new ActionListener() {
+        btnNewButton_2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 panel4.removeAll();
@@ -139,9 +202,8 @@ public class page_2 extends JPanel{
         });
 
         main.setLayout(new GridLayout(1,2));
-        panel3.setBorder(new LineBorder(Color.ORANGE,3)); // 패널 크기를 육안으로 확인하기 위해 경계선 그어줌
+        //panel_3.setBorder(new LineBorder(Color.ORANGE,3)); // 패널 크기를 육안으로 확인하기 위해 경계선 그어줌
         panel4.setBorder(new LineBorder(Color.white,3)); // 위와 동일
-        main.add(panel3);
         main.add(panel4);
         this.add(main);
     }

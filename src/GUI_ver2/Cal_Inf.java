61530c2214fdf74280cc6c55d92ab33667810352
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2021-12-03 20:37:00 +0900

    3장 하단에 스케줄 리스트 표시 (#15)
    * page_3: 스케줄 표시
    
    * Cal_inf에서 텍스트 정보 받아옴

--- src/GUI_ver2/Cal_Inf.java
+++ src/GUI_ver2/Cal_Inf.java
@@ -4,30 +4,53 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 public class Cal_Inf extends JPanel{
-    public Cal_Inf(){
-        JLabel goal = new JLabel("목표");
-        JLabel time = new JLabel("시간");
-        JLabel how = new JLabel("측정방법");
-        JTextField goal_txt = new JTextField(10);
-        JTextField time_txt = new JTextField(10);
-        JTextField how_txt = new JTextField(10);
+    private JTextField goal_txt;
+    private JTextField time_txt;
+    private JTextField how_txt;
 
-        setLayout(new GridLayout(3,2,5,5));
-        goal.setBorder(new LineBorder(Color.ORANGE));
+    public Cal_Inf(){
+        setLayout(null);
+
+        JLabel goal_label = new JLabel("목표", JLabel.CENTER);
+        goal_label.setBounds(10, 0, 50, 18);
+
+        JLabel time_label = new JLabel("시간", JLabel.CENTER);
+        time_label.setBounds(10, 20, 50, 18);
+
+        JLabel how_label = new JLabel("측정방법", JLabel.CENTER);
+        how_label.setBounds(10, 40, 50, 18);
+
+        goal_txt = new JTextField(10);
+        goal_txt.setBounds(70, 0, 200, 18);
+
+        time_txt = new JTextField(10);
+        time_txt.setBounds(70, 20, 200, 18);
+
+        how_txt = new JTextField(10);
+        how_txt.setBounds(70, 40, 200, 18);
+
+        //goal_label.setBorder(new LineBorder(Color.ORANGE));
         goal_txt.setBorder(new LineBorder(Color.ORANGE));
-        time.setBorder(new LineBorder(Color.ORANGE));
+        //time_label.setBorder(new LineBorder(Color.ORANGE));
         time_txt.setBorder(new LineBorder(Color.ORANGE));
-        how.setBorder(new LineBorder(Color.ORANGE));
+        //how_label.setBorder(new LineBorder(Color.ORANGE));
         how_txt.setBorder(new LineBorder(Color.ORANGE));
 
-        add(goal);
+        add(goal_label);
         add(goal_txt);
-        add(time);
+        add(time_label);
         add(time_txt);
-        add(how);
+        add(how_label);
         add(how_txt);
 
         setBorder(new LineBorder(Color.BLUE));
     }
 
+    public String getGoal() {
+        return goal_txt.getText();
+    }
+
+    public String getTime() {
+        return time_txt.getText();
+    }
 }
